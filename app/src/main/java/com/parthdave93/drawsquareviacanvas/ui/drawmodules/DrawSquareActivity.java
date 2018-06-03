0b8b5a0c4fe754ea8a606f85d0b51d5c8c8bd86c
Parth Dave <parth.dave93@gmail.com>
2018-06-03 16:00:53 +0530

    moving items to package

--- app/src/main/java/com/parthdave93/drawsquareviacanvas/ui/drawmodules/DrawSquareActivity.java
+++ app/src/main/java/com/parthdave93/drawsquareviacanvas/ui/drawmodules/DrawSquareActivity.java
@@ -1,7 +1,9 @@
-package com.parthdave93.drawsquareviacanvas;
+package com.parthdave93.drawsquareviacanvas.ui.drawmodules;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.parthdave93.drawsquareviacanvas.R;
 
 public class DrawSquareActivity extends AppCompatActivity {
 
